3e8598c4516f24a0e91ce3c370f0494d8160e893
lostmarison <146553120+lostmarison@users.noreply.github.com>
2024-10-02 14:33:10 +0800

    Update and rename process2.py to process.py

--- python/process.py
+++ python/process.py
@@ -1,3 +1,27 @@
+'''
+# 工作簿--workbook
+# 表单-sheet
+# 单元格-cell
+# 行:row
+# 列:col
+import xlrd  
+# 打开一个 Excel 文件  
+workbook = xlrd.open_workbook('文件路径.xls')  
+# 获取所有工作表的名称  
+sheet_names = workbook.sheet_names()  
+# 通过索引或名称获取工作表  
+sheet1 = workbook.sheet_by_index(0)  # 通过索引获取第一个工作表  
+# 读取单元格数据  
+cell_value = sheet1.cell_value(rowx=0, colx=0)  # 读取第一行第一列的数据
+# 读取行数
+rows = sheet1.nrows 
+# 读取列数
+cols = sheet1.ncols
+# 读取行元素(整行元素)
+sheet1.row_value(i)
+# 读取列元素
+sheet1.col_values(j)
+'''
 import xlrd
 
 
